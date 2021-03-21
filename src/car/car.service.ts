537e1d2449675ec3b6cc54426fa709303c4a0908
MDRezaulHasan <rezaulhasan0168@gmail.com>
2021-03-21 14:39:50 +0100

    crud oparetion is done

---
+++ src/car/car.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, HttpException } from '@nestjs/common';
+import {CARS} from './cars.mock';
+@Injectable()
+export class CarService {
+    private cars = CARS;
+    public getCars(){
+        return this.cars;
+    }
+     public postCar(car){
+        return this.cars.push(car);
+    }
+     public getCarById(id: number): Promise<any>{
+         const carId = Number(id);
+         return new Promise((resolve)=>{
+         const car = this.cars.find((car) => car.id === id);
+            if(car){
+                throw new HttpException("Not Found",404 );
+            }
+            return resolve(car);
+         });    
+    }
+     public deleteCarById(id): Promise<any>{
+             const carId = Number(id);
+         return new Promise((resolve)=>{
+            const index = this.cars.findIndex((car) => car.id === carId);
+         if(index){
+            throw new HttpException("Not Found",404 );
+        }
+        //for deleting item from a array
+        this.cars.splice( index,1);
+        return resolve(this.cars);
+         });  
+    }
+    public putCarById(id: number,propertyName: string, propertyValue: string): Promise<any>{
+        const carId = Number(id);
+        return new Promise((resolve)=>{
+            const index = this.cars.findIndex((car) => car.id === carId);
+            if(index === -1){
+                throw new HttpException("Not Found",404 );
+            }
+        //updating array
+        this.cars[index][propertyName] = propertyValue;
+        return resolve(this.cars);
+         });  
+    }
+}
